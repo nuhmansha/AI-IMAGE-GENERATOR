ce84cf8a9e42210956f6c3203ced770d49adea5c
nuhmansha <nuhmanibnunazar@gmail.com>
2024-06-21 20:48:56 +0530

    Initial commit

---
+++ src/Components/ImageGenerator/ImageGenerator.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './ImageGenerator.css'
+import default_image from '../../assets/img-d99cgrj2qQsYh4JnmYEJT0GA-140.png'
+const ImageGenerator = () => {
+  return (
+    <div className='ai-image-generator'>
+        
+    </div>
+  )
+}
+
+export default ImageGenerator
