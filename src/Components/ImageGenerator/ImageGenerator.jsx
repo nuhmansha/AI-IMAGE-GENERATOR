a915f0c02936b3e87037106a31f73e6a008cf413
nuhmansha <nuhmanibnunazar@gmail.com>
2024-06-22 20:36:34 +0530

    some changes

--- src/Components/ImageGenerator/ImageGenerator.jsx
+++ src/Components/ImageGenerator/ImageGenerator.jsx
@@ -4,7 +4,12 @@ import default_image from '../../assets/img-d99cgrj2qQsYh4JnmYEJT0GA-140.png'
 const ImageGenerator = () => {
   return (
     <div className='ai-image-generator'>
-        
+        <div className="header">Ai image <span>generator</span></div>
+        <div className="img-loading">
+            <div className="image">
+                <img src={default_image} alt="" />
+            </div>
+        </div>
     </div>
   )
 }
